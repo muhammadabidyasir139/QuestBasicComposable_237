73cfc1f0ddd18257af2c5a475e0debcd9098cf21
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2025-10-16 14:35:38 +0700

    membuat Box didalam column

--- app/src/main/java/com/example/questbasiccomposable_237/TugasCopy.kt
+++ app/src/main/java/com/example/questbasiccomposable_237/TugasCopy.kt
@@ -131,6 +131,14 @@ fun TataLetakBoxColumnRow(modifier: Modifier) {
         }
         Spacer(modifier = Modifier.height(10.dp))
 
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .background(color = Color.Cyan),
+            contentAlignment = Alignment.Center
+        )
+
 
 
 
