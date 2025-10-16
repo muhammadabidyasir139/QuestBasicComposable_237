7a6e317057391bede71e0e06f29e64e33a775d1f
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2025-10-16 14:49:11 +0700

    menambah import yang diperlukan

--- app/src/main/java/com/example/questbasiccomposable_237/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_237/MainActivity.kt
@@ -1,5 +1,19 @@
 package com.example.questbasiccomposable_237
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.questbasiccomposable.ui.theme.QuestBasicComposableTheme
+import com.example.questbasiccomposable.ui.TataLetakBoxColumnRow
+import androidx.compose.material3.Text
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
